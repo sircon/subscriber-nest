a76842bd69684c4740d2113fa4b4c973c83e3f03
Miguel Correia <miguelncorreia@gmail.com>
2026-01-19 22:27:43 +0100

    feat: US-005 - Update backend API service response

--- apps/backend/apps/api/src/app.service.ts
+++ apps/backend/apps/api/src/app.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'SubscriberNest API';
+    return 'AudienceSafe API';
   }
 
   health(): { status: string } {
