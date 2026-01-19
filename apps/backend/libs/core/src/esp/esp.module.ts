121faf2ca6d2a8dae77bb9c4688c720de06aeb2c
Miguel Correia <miguelncorreia@gmail.com>
2026-01-19 10:25:51 +0100

    feat: [US-016] - Register all 14 new ESP connectors in ESP module
    Import and register all new ESP connectors in the NestJS module:
    - ActiveCampaign, Brevo, Campaign Monitor, Constant Contact
    - Customer.io, Email Octopus, Ghost, Iterable
    - MailerLite, Omeda, PostUp, Sailthru
    - SendGrid, SparkPost
    
    All connectors are added to both providers and exports arrays
    for proper dependency injection throughout the application.

--- apps/backend/libs/core/src/esp/esp.module.ts
+++ apps/backend/libs/core/src/esp/esp.module.ts
@@ -3,10 +3,64 @@ import { HttpModule } from '@nestjs/axios';
 import { BeehiivConnector } from './beehiiv.connector';
 import { MailchimpConnector } from './mailchimp.connector';
 import { KitConnector } from './kit.connector';
+import { ActiveCampaignConnector } from './active-campaign.connector';
+import { BrevoConnector } from './brevo.connector';
+import { CampaignMonitorConnector } from './campaign-monitor.connector';
+import { ConstantContactConnector } from './constant-contact.connector';
+import { CustomerIoConnector } from './customer-io.connector';
+import { EmailOctopusConnector } from './email-octopus.connector';
+import { GhostConnector } from './ghost.connector';
+import { IterableConnector } from './iterable.connector';
+import { MailerLiteConnector } from './mailerlite.connector';
+import { OmedaConnector } from './omeda.connector';
+import { PostUpConnector } from './postup.connector';
+import { SailthruConnector } from './sailthru.connector';
+import { SendGridConnector } from './sendgrid.connector';
+import { SparkPostConnector } from './sparkpost.connector';
 
 @Module({
   imports: [HttpModule],
-  providers: [BeehiivConnector, KitConnector, MailchimpConnector],
-  exports: [BeehiivConnector, KitConnector, MailchimpConnector],
+  providers: [
+    // Existing connectors
+    BeehiivConnector,
+    KitConnector,
+    MailchimpConnector,
+    // New connectors
+    ActiveCampaignConnector,
+    BrevoConnector,
+    CampaignMonitorConnector,
+    ConstantContactConnector,
+    CustomerIoConnector,
+    EmailOctopusConnector,
+    GhostConnector,
+    IterableConnector,
+    MailerLiteConnector,
+    OmedaConnector,
+    PostUpConnector,
+    SailthruConnector,
+    SendGridConnector,
+    SparkPostConnector,
+  ],
+  exports: [
+    // Existing connectors
+    BeehiivConnector,
+    KitConnector,
+    MailchimpConnector,
+    // New connectors
+    ActiveCampaignConnector,
+    BrevoConnector,
+    CampaignMonitorConnector,
+    ConstantContactConnector,
+    CustomerIoConnector,
+    EmailOctopusConnector,
+    GhostConnector,
+    IterableConnector,
+    MailerLiteConnector,
+    OmedaConnector,
+    PostUpConnector,
+    SailthruConnector,
+    SendGridConnector,
+    SparkPostConnector,
+  ],
 })
-export class EspModule { }
+export class EspModule {}
